d064e8cd1a5c6e85140f149b3d824a9933c4b511
Adam Lehrer <alehrer@box.com>
2023-02-27 13:53:25 -0800

    adding new package json

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { Options } from "@mikro-orm/core";
 import path from 'path';
+import { User } from "./entities/User";
 
 const config: Options = {
     migrations:{
         path: path.join(__dirname,'./migrations'),
         glob: "!(*.d).{js,ts}",
     },
-    entities: [Post],
+    entities: [Post,User],
     dbName: "wheretogo",
     user: "postgres",
     password: 'password',
