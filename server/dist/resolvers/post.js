50cd408873d60992c4b139e602b3cc4f694d3e3d
Adam <lehreradam@yahoo.com>
2023-04-04 19:12:16 -0700

    adding resolvers

--- server/dist/resolvers/post.js
+++ server/dist/resolvers/post.js
@@ -80,10 +80,11 @@ let PostResolver = class PostResolver {
     ) creator
     from post p
     inner join public.user u on u.id = p."creatorId"
-    ${cursor ? `where p."creadtedAt" <$2` : ''}
+    ${cursor ? `where p."createdAt" <$2` : ''}
     order by p."createdAt" DESC
     limit $1
     `, replacements);
+        console.log(posts);
         return {
             posts: posts.slice(0, realLimit),
             hasMore: posts.length === realLimitPlusOne,
--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -89,13 +89,13 @@ export class PostResolver {
     ) creator
     from post p
     inner join public.user u on u.id = p."creatorId"
-    ${cursor ? `where p."creadtedAt" <$2` : ''}
+    ${cursor ? `where p."createdAt" <$2` : ''}
     order by p."createdAt" DESC
     limit $1
     `,
       replacements
     );
-
+      console.log(posts)
     return {
       posts: posts.slice(0, realLimit),
       hasMore: posts.length === realLimitPlusOne,
